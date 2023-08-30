4b8820bb6da405b99cd340da05969159157c8afd
Sugakesshaa <sugakesshaa@gmail.com>
2023-08-30 23:38:02 +0800

    sm6375-common: Declare support for oscar and larry
    Change-Id: I935b82f5a5f76b72912f967d4a5050a449eee01c

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "cupidr ice lemonade lemonadep lunaa martini porsche ziti"
+write_headers "larry oscar"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
