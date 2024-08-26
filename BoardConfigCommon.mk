9c6db0d165b540b1d44dd33fee564dbb42ebd6df
LuK1337 <priv.luk@gmail.com>
2024-08-27 00:00:55 +0200

    sm6375-common: Switch to common device_framework_matrix.xml
    Change-Id: Idc99a59a377bb3284634a62c0a1ae545bcd4e1e2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,7 +73,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
+    hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
