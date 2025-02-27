05577975097df27a41f93d687879ed7947f3710b
Sugakesshaa <sugakesshaa@pixelos.net>
2025-02-27 22:01:57 +0800

    sm6375-common: Upgrade to RMX3660_15.0.0.510(EX01)
    Change-Id: I730103867685a1bc664c6515ccc760afacd7884f

--- extract-files.py
+++ extract-files.py
@@ -63,8 +63,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
-    'vendor/etc/media_holi/video_system_specs.json': blob_fixup()
-        .regex_replace('"max_retry_alloc_output_timeout": 2000,', '"max_retry_alloc_output_timeout": 0,'),
+    ('vendor/etc/media_blair/video_system_specs.json', 'vendor/etc/media_holi/video_system_specs.json'): blob_fixup()
+        .regex_replace('"max_retry_alloc_output_timeout": 10000,', '"max_retry_alloc_output_timeout": 0,'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('IGNORED_IRQ=19,21,38$', 'IGNORED_IRQ=19,21,38,209,218'),
     'vendor/etc/qcril_database/upgrade/config/*.sql': blob_fixup()
