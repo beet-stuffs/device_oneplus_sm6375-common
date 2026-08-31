4410bdc42377d8896141a9c305aa5ddfc91355d9
Sugakesshaa <sugakesshaa@pixelos.net>
2026-09-01 01:59:35 +0800

    Revert "sm6375-common: power-libperfmgr: Update included soong namespaces"
    This reverts commit 8111260dcc106e4486be194f33d22c9818f270bf.

--- common.mk
+++ common.mk
@@ -278,8 +278,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/oplus \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/google/pixel/pixelstats \
-    hardware/google/pixel/power-libperfmgr \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
